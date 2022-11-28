1dc47a000ddd09723823f11e9c4c140c22022ceb
Sydney Haynes <sydneyhaynes22@gmail.com>
2022-11-28 16:07:49 -0500

    added alerts and isNaN

--- script.js
+++ script.js
@@ -6,26 +6,54 @@ function generatePassword(){
   var letters = "abcdefghijklmnopqrstuvwxyz";
   var numbers = "0987654321";
   var upLetters = letters.toUpperCase();
+  var chooseFrom = "";
 
-  var isUppercase = confirm("Would you like to use uppercase")
-  var isLowercase
-  var isSpecial
-  var isNumber
+  
+
+  var islength = prompt("What is the length of the password?")
+
+  if (islength > 128 ) {
+    alert("Password must be between 8 to 128 characters")   
+    return; 
+  }
+  if (islength < 8) {
+  alert("Password must be between 8 to 128 characters")
+  return;
+  }
+
+  console.log (isNaN(islength))
+  if(isNaN(islength)){
+    alert("Please use a number!")
+    return;
+  }
+
+
+  var isUppercase = confirm("Would you like to use uppercase letters?")
+  var isLowercase = confirm("Would you like to use lowercase letters?")
+  var isSpecial = confirm("Would you like to use special characters?")
+  var isNumber = confirm("Would you like to use numbers?")
+
+
+ 
 
   if (isUppercase) {
-    console.log("uppercase")
+    console.log("uppercase");
+    chooseFrom += upLetters;
   }
 
   if (isLowercase) {
-    console.log("lowercase")
+    console.log("lowercase");
+    chooseFrom += letters;
   }
 
   if (isSpecial) {
-console.log("special")
+    console.log("special");
+    chooseFrom += potentialSpecial;
   }
 
   if (isNumber) {
     console.log("number")
+    chooseFrom += numbers
   }
 }
 
